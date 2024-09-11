67fa6e208e3d45cb50283a12128ac214f35a9d3c
Irene031600 <desquitadoirene_bscs@plmun.edu.ph>
2024-09-11 14:29:58 +0800

    Add files via upload

---
+++ grp5_desquitado_ex1.py
@@ -0,0 +1,16 @@
+#My first simple script
+
+print ("Hello,I,m Irene Desquitado! BSCS 3F") age = 24 name = "Irene"
+
+print("Age: ",age) print("Name: ",name)
+
+Elopre=" Elopre"
+angela="angela"
+judith= "judith"
+jane=" jane"
+
+age, name, gender = 22, "Irene", "female" print ("Age:",age) print ("Name:", name)
+
+print ("Gender:",gender)
+
+age, edad, num = 24,24, 24 print(age, edad, num)
